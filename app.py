663ca6cd7bb6124a7a4f5896114b7cdd44c61e45
stahlkopf <btaylertech@gmail.com>
2015-11-08 18:40:17 -0500

    Changing json response

--- app.py
+++ app.py
@@ -12,24 +12,11 @@ from flask import Flask, make_response, redirect, abort
 from flask.ext.restful import Resource, Api, reqparse
 from flask.ext.restful.representations.json import output_json
 
-from newspaper.parsers import Parser
 from lxml.html.clean import Cleaner
 from html2text import HTML2Text
 
 
-def clean_article_html(cls, node):
-    article_cleaner = Cleaner()
-    article_cleaner.javascript = True
-    article_cleaner.style = True
-    article_cleaner.allow_tags = ['a', 'span', 'p', 'br', 'strong', 'b', 'ul', 'ol', 'li',
-                                  'em', 'i', 'code', 'pre', 'blockquote', 'h1',
-                                  'h2', 'h3', 'h4', 'h5', 'h6']
-    article_cleaner.remove_unknown_tags = False
-    return article_cleaner.clean_html(node)
-
-Parser.clean_article_html = classmethod(clean_article_html)
-
-import newspaper
+from newspaper import Article
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -51,9 +38,7 @@ class MyApi(Api):
 
 api = MyApi(app)
 
-config = newspaper.Config()
-config.is_memoize_articles = False
-config.keep_article_html = True
+
 
 article_parser = reqparse.RequestParser()
 article_parser.add_argument('url', type=unicode, help='The url of the site to scrape')
@@ -68,7 +53,7 @@ class ArticleSimple(Resource):
         args = article_parser.parse_args()
         url = args['url']
         output_format = args['format']
-        article = newspaper.build_article(url, config)
+        article = Article(url)
         article.download()
         if article.html == "":
             abort(404)
@@ -81,7 +66,7 @@ class ArticleSimple(Resource):
             if markdownify:
                 h = HTML2Text(baseurl=args['url'])
                 h.body_width = 0
-                text = h.handle(article.article_html)
+                text = h.handle(article.html)
 
             data = {
                 'url': article.url,
@@ -89,7 +74,7 @@ class ArticleSimple(Resource):
                 'top_image': article.top_img,
                 'images': [x for x in article.imgs],
                 'text': text,
-                'html': article.article_html,
+                'html': article.html,
                 'keywords': article.keywords,
                 'authors': article.authors,
                 'summary': article.summary,
@@ -144,29 +129,13 @@ class ArticleSimple(Resource):
 
                 return output_text(output, 200, {'Content-Type': 'text'})
 
-site_parser = reqparse.RequestParser()
-site_parser.add_argument('url', type=unicode, help='The url of the site to scrape')
 
 
-class SourceSimple(Resource):
 
-    def get(self):
-        args = site_parser.parse_args()
-        source = newspaper.build(args['url'], config)
-        return {
-            'domain': source.domain,
-            'logo_url': source.logo_url,
-            'favicon': source.favicon,
-            'brand': source.brand,
-            'description': source.description,
-            'categories': [unicode(x.url) for x in source.categories],
-            'feeds': [unicode(x.url) for x in source.feeds],
-            'articles': [unicode(x.url) for x in source.articles],
-        }
 
 
 api.add_resource(ArticleSimple, '/article')
-api.add_resource(SourceSimple, '/source')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
